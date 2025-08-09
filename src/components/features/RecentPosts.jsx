5011dea821cf12261f9086af7761e8ef4cbcd365
hanruochong <asong6824@outlook.com>
2025-08-09 18:24:56 +0900

    modify

--- src/components/features/RecentPosts.jsx
+++ src/components/features/RecentPosts.jsx
@@ -28,7 +28,7 @@ export default function RecentPosts() {
     }
 
     return (
-        <section id="recentposts" className="mt-32">
+        <section id="recentposts" className="mt-32 mb-16">
             <div className="container mx-auto px-4">
                 <h2 className="text-3xl md:text-4xl font-bold text-center mb-12">
                     最新博文
